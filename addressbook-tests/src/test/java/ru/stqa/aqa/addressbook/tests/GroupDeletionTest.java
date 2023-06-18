eda47b44a97ea118b34e0c3322bf44edfa59b5fd
Mariya Kolesnikova <mkolesnikova.kld@gmail.com>
2023-06-18 10:56:52 +0200

    implemented precondition checking when creating and modifying a group

--- addressbook-tests/src/test/java/ru/stqa/aqa/addressbook/tests/GroupDeletionTest.java
+++ addressbook-tests/src/test/java/ru/stqa/aqa/addressbook/tests/GroupDeletionTest.java
@@ -1,12 +1,16 @@
 package ru.stqa.aqa.addressbook.tests;
 
 import org.testng.annotations.*;
+import ru.stqa.aqa.addressbook.model.GroupData;
 
 public class GroupDeletionTest extends TestBase{
 
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationsHelper().gotoGroupPage();
+    if(!app.getGroupHelper().isThereAGroup()){
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroup();
     app.getGroupHelper().returnToGroupPage();
