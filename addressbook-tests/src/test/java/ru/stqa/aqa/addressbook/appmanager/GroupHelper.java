bbaae3b083896c60a7920863965ef4edbdcd5922
Mariya Kolesnikova <mkolesnikova.kld@gmail.com>
2023-07-08 16:22:21 +0200

    Collections added to the tests for groups

--- addressbook-tests/src/test/java/ru/stqa/aqa/addressbook/appmanager/GroupHelper.java
+++ addressbook-tests/src/test/java/ru/stqa/aqa/addressbook/appmanager/GroupHelper.java
@@ -2,7 +2,11 @@ package ru.stqa.aqa.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import ru.stqa.aqa.addressbook.model.GroupData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroupHelper extends HelperBase{
 
@@ -29,8 +33,8 @@ public class GroupHelper extends HelperBase{
         click(By.name("submit"));
     }
 
-    public void selectGroup() {
-        click(By.name("selected[]"));
+    public void selectGroup(int index) {
+        wd.findElements(By.name("selected[]")).get(index).click();
     }
 
     public void deleteSelectedGroup() {
@@ -58,5 +62,17 @@ public class GroupHelper extends HelperBase{
 
     public int getGroupCount() {
         return wd.findElements(By.name("selected[]")).size();
+    }
+
+    public List<GroupData> getGroupList() {
+        List<GroupData> groups = new ArrayList<GroupData>();
+        List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+        for (WebElement element : elements) {
+            String name = element.getText();
+            String id = element.findElement(By.tagName("input")).getAttribute("value");
+            GroupData group = new GroupData(id, name, null, null);
+            groups.add(group);
+        }
+        return groups;
     }
 }
