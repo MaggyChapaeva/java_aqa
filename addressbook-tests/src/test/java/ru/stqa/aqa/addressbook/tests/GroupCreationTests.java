f98a8383299bb1b754c803e1307555262ebb7fdd
Mariya Kolesnikova <mkolesnikova.kld@gmail.com>
2023-07-21 12:18:18 +0200

    Set and unique IDs in groups

--- addressbook-tests/src/test/java/ru/stqa/aqa/addressbook/tests/GroupCreationTests.java
+++ addressbook-tests/src/test/java/ru/stqa/aqa/addressbook/tests/GroupCreationTests.java
@@ -3,35 +3,23 @@ package ru.stqa.aqa.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.aqa.addressbook.model.GroupData;
-
-import java.util.Comparator;
-import java.util.List;
+import java.util.Set;
 
 public class GroupCreationTests extends TestBase{
 
   @Test
   public void testGroupCreation() throws Exception {
     app.goTo().groupPage();
-    List<GroupData> before = app.group().list();
+    Set<GroupData> before = app.group().all();
     GroupData group = new GroupData().withName("test1");
     app.group().create(group);
-    List<GroupData> after = app.group().list();
+    Set<GroupData> after = app.group().all();
     Assert.assertEquals(after.size(), before.size() + 1);
 
 
-    /*int max = 0;
-    for (GroupData g : after) {
-      if(g.getId() > max){
-        max = g.getId();
-      }
-    }*/
-    //Comparator<? super GroupData> byId = (Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
-
-    group.withId(after.stream().max(Comparator.comparingInt(GroupData::getId)).get().getId());
+    group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());
     before.add(group);
-    Comparator<? super GroupData> byId = Comparator.comparingInt(GroupData::getId);
-    before.sort(byId);
-    after.sort(byId);
+
     Assert.assertEquals(before, after);
   }
 }
