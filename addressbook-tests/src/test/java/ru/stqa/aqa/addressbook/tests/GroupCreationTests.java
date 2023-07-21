7cd1c739eb1cb9c6b5226c33763aed404e059da5
Mariya Kolesnikova <mkolesnikova.kld@gmail.com>
2023-07-21 11:47:15 +0200

    implemented fluent interface for groups

--- addressbook-tests/src/test/java/ru/stqa/aqa/addressbook/tests/GroupCreationTests.java
+++ addressbook-tests/src/test/java/ru/stqa/aqa/addressbook/tests/GroupCreationTests.java
@@ -13,7 +13,7 @@ public class GroupCreationTests extends TestBase{
   public void testGroupCreation() throws Exception {
     app.goTo().groupPage();
     List<GroupData> before = app.group().list();
-    GroupData group = new GroupData("test1", null, null);
+    GroupData group = new GroupData().withName("test1");
     app.group().create(group);
     List<GroupData> after = app.group().list();
     Assert.assertEquals(after.size(), before.size() + 1);
@@ -27,7 +27,7 @@ public class GroupCreationTests extends TestBase{
     }*/
     //Comparator<? super GroupData> byId = (Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
 
-    group.setId(after.stream().max(Comparator.comparingInt(GroupData::getId)).get().getId());
+    group.withId(after.stream().max(Comparator.comparingInt(GroupData::getId)).get().getId());
     before.add(group);
     Comparator<? super GroupData> byId = Comparator.comparingInt(GroupData::getId);
     before.sort(byId);
