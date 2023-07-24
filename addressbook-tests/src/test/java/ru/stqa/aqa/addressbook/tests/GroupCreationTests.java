f01e231bbc41a0cddf2ddd86df53ba7659837c3c
Mariya Kolesnikova <mkolesnikova.kld@gmail.com>
2023-07-24 13:43:11 +0200

    6.5 testGroupCreation Test ignored

--- addressbook-tests/src/test/java/ru/stqa/aqa/addressbook/tests/GroupCreationTests.java
+++ addressbook-tests/src/test/java/ru/stqa/aqa/addressbook/tests/GroupCreationTests.java
@@ -3,48 +3,70 @@ package ru.stqa.aqa.addressbook.tests;
 import org.testng.annotations.*;
 import ru.stqa.aqa.addressbook.model.GroupData;
 import ru.stqa.aqa.addressbook.model.Groups;
-
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class GroupCreationTests extends TestBase{
+public class GroupCreationTests extends TestBase {
 
-  @DataProvider
-  public Iterator<Object[]> validGroups(){
-    List<Object[]> list = new ArrayList<>();
-    list.add(new Object[] {new GroupData().withName("test1").withHeader("header1").withFooter("footer1")});
-    list.add(new Object[] {new GroupData().withName("test2").withHeader("header2").withFooter("footer2")});
-    list.add(new Object[] {new GroupData().withName("test3").withHeader("header3").withFooter("footer3")});
-    return list.iterator();
-  }
-  @Test(dataProvider = "validGroups")
-  public void testGroupCreation(GroupData group) {
-      app.goTo().groupPage();
-      Groups before = app.group().all();
-      app.group().create(group);
-      assertThat(app.group().count(), equalTo(before.size() + 1));
+    @DataProvider
+    public Iterator<Object[]> validGroups() throws IOException {
 
-      Groups after = app.group().all();
+        List<Object[]> list = new ArrayList<Object[]>();
+        String filePath = "src/test/resources/groups.csv";
+        File file = new File(filePath);
 
-      assertThat(after, equalTo(
-              before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+        /*if (file.exists() && file.isFile()) {
+            System.out.println("Путь к файлу верный");
+        } else {
+            System.out.println("Неверный путь к файлу");
+        }*/
 
-  }
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String line = reader.readLine();
 
-  @Test
-  public void testBadGroupCreation() throws Exception {
-    app.goTo().groupPage();
-    Groups before = app.group().all();
-    GroupData group = new GroupData().withName("test1'");
-    app.group().create(group);
-    assertThat(app.group().count(), equalTo(before.size()));
-    Groups after = app.group().all();
+        /*if(line!=null){
+            System.out.println(line);
+        }*/
 
+        while (line != null) {
+            String[] split = line.split(";");
+            list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+            line = reader.readLine();
+        }
+        return list.iterator();
 
-    assertThat(after, equalTo(before));
-  }
+        //Предыдущая версия рабочая
+        /*List<Object[]> list = new ArrayList<>();
+        list.add(new Object[] {new GroupData().withName("test1").withHeader("header1").withFooter("footer1")});
+        list.add(new Object[] {new GroupData().withName("test2").withHeader("header2").withFooter("footer2")});
+        list.add(new Object[] {new GroupData().withName("test3").withHeader("header3").withFooter("footer3")});
+        return list.iterator();*/
+    }
+
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group) {
+        app.goTo().groupPage();
+        Groups before = app.group().all();
+        app.group().create(group);
+        assertThat(app.group().count(), equalTo(before.size() + 1));
+        Groups after = app.group().all();
+        assertThat(after, equalTo(before.withAdded
+                                 (group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+
+    }
+
+    @Test
+    public void testBadGroupCreation() throws Exception {
+        app.goTo().groupPage();
+        Groups before = app.group().all();
+        GroupData group = new GroupData().withName("test1'");
+        app.group().create(group);
+        assertThat(app.group().count(), equalTo(before.size()));
+        Groups after = app.group().all();
+        assertThat(after, equalTo(before));
+    }
 }
--- addressbook-tests/src/test/java/ru/stqa/aqa/addressbook/tests/TestBase.java
+++ addressbook-tests/src/test/java/ru/stqa/aqa/addressbook/tests/TestBase.java
@@ -7,7 +7,7 @@ import ru.stqa.aqa.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(Browser.CHROME.browserName());
+    protected static final ApplicationManager app = new ApplicationManager(Browser.FIREFOX.browserName());
 
     @BeforeSuite(alwaysRun = true)
     public void setUp() throws Exception {
